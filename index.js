c3f274d6a7a423329183c66616f73064b68738eb
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-08-11 22:44:20 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+function superbowlWin(jsArray) {
+    const winObj = jsArray.find(obj => obj.result === 'W');
+    if (winObj) {
+        return winObj.year;
+    } else { 
+        return undefined;
+    }
+}
